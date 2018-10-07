4b2d25f67ee6027900f95a18119672d9231865b4
Jacek Szymborski <jacek.szymborski@gmail.com>
2018-10-07 15:12:40 +0200

    Listy

--- zjazd_1/zadanie_17.py
+++ zjazd_1/zadanie_17.py
@@ -1,10 +1,10 @@
 lista = []
 i = 1
-iteracje = int(input("Wprowadz wielkosc listy:"))
+iteracje = int(input("Wprowadz wielkosc listy: "))
 
 print(lista,i)
 while i <= iteracje:
-    liczba = int(input("Wprowadz liczbe:"))
+    liczba = int(input("Wprowadz liczbe: "))
     lista.append(liczba)
     i += 1
     print(lista,i)
