e6f08c87f166f16fa35da6c26897d04f3ff7fced
Jacek Szymborski <jacek.szymborski@gmail.com>
2018-10-07 16:56:04 +0200

    Z1D2 - koniec dnia

--- zjazd_1/zadanie_19.py
+++ zjazd_1/zadanie_19.py
@@ -5,12 +5,12 @@ lista_w = []
 
 for liczba in range(0,101):
     lista_w.append(liczba)
+    print(liczba)
     if liczba % 3 == 0 or liczba % 5 == 0:
         lista_35.append(liczba)
     if liczba % 3 == 0:
         lista_3.append(liczba)
     if liczba % 5 == 0:
         lista_5.append(liczba)
-
-print(lista_w)
+print()
 print(len(lista_3), len(lista_5), len(lista_35))
