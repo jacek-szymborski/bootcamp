78ff5cb8326def044bd07ffadc1edb8ba5e520f8
Jacek Szymborski <jacek.szymborski@gmail.com>
2018-11-03 16:59:43 +0100

    Trzeci dzień - niedokończone zadanie 3, rozpoczete zadanie 4

--- zjazd_3/zadanie_4.py
+++ zjazd_3/zadanie_4.py
@@ -1,1 +1,32 @@
-def test_basket():
+def test_Basket():
+    basket = Basket()
+    assert basket.produkty == []
+
+def test_Product():
+    pass
+
+# def test_basket_add_product():
+#     assert basket.add_product(product,5) ==
+
+
+class Basket:
+
+    def __init__(self):
+        self.produkty = []
+
+    def add_product(self):
+        pass
+
+    def count_total_price(self):
+        pass
+
+    def generate_report(self):
+        pass
+
+
+class Product:
+
+    def __init__(self, id, name, unit_price):
+        self.id = id
+        self.name = name
+        self.unit_price = unit_price
